b68e420e33f7b44d8b870e04230c8f70f631ccbc
Jason Wang <jason@Jasons-MacBook-Pro.local>
2022-04-20 14:42:43 -0700

    Updates

---
+++ precache-manifest.a1ae6a45effba85e21266b352340ccca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627822c4839c7d14d15c7cd50d0bdff5",
+    "url": "/hawk198723/react-form/index.html"
+  },
+  {
+    "revision": "e8dfcc6ab32c71756635",
+    "url": "/hawk198723/react-form/static/css/main.10078b94.chunk.css"
+  },
+  {
+    "revision": "4893809409bd7aa4c556",
+    "url": "/hawk198723/react-form/static/js/2.5aaf71f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hawk198723/react-form/static/js/2.5aaf71f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8dfcc6ab32c71756635",
+    "url": "/hawk198723/react-form/static/js/main.4b719dac.chunk.js"
+  },
+  {
+    "revision": "d2d9d0f5d06f0518d617",
+    "url": "/hawk198723/react-form/static/js/runtime-main.a1b35d9d.js"
+  }
+]);
